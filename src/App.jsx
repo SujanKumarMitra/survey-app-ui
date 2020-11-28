51511f1809f40233337b9cb926955a518688d04a
Sujan Kumar Mitra <mitrakumarsujan@gmail.com>
2020-11-28 23:54:08 +0530

    code refactoring
    extracted options to OptionInput

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import TimeField from './components/QuestionFields/TimeField/TimeField';
 import FormResponse from './pages/FormResponse/FormResponse';
 import FormResponseData from './pages/FormResponseData/FormResponseData';
+
+import TextField from './components/QuestionFields/TextField/TextField';
+import DateField from './components/QuestionFields/DateField/DateField';
+import TimeField from './components/QuestionFields/TimeField/TimeField';
+import CheckBoxField from './components/QuestionFields/CheckBoxField/CheckBoxField';
+import RadioField from './components/QuestionFields/RadioField/RadioField';
+
 const App = (props) => {
     return (
         <BrowserRouter>
@@ -13,7 +19,11 @@ const App = (props) => {
                     <FormResponseData />
                 </Route>
                 <Route exact path="/">
+                    <TextField />
+                    <DateField />
                     <TimeField />
+                    <CheckBoxField />
+                    <RadioField />
                 </Route>
             </Switch>
         </BrowserRouter>
