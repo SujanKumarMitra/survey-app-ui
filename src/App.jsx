740cdb7ba99cb1fc926a9c813cc9944de00817bb
Sujan Kumar Mitra <mitrakumarsujan@gmail.com>
2020-11-29 22:26:16 +0530

    route cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import FormCreateSuccess from './components/FormCreateSuccess/FormCreateSuccess';
 import FormCreate from './pages/FormCreate/FormCreate';
 import FormResponse from './pages/FormResponse/FormResponse';
 import FormResponseData from './pages/FormResponseData/FormResponseData';
@@ -15,11 +14,8 @@ const App = (props) => {
                 <Route exact path="/responses/">
                     <FormResponseData />
                 </Route>
-                <Route exact path="/form/create">
+                <Route exact path="/form">
                     <FormCreate />
-                </Route>
-                <Route exact path="/">
-                    <FormCreateSuccess />
                 </Route>
             </Switch>
         </BrowserRouter>
